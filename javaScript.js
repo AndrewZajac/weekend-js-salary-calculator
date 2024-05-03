6b77dc2e9f7e0adb9d61ec29589abf04cc4553b7
AndrewZajac <andrewzajac05@gmail.com>
2024-05-03 17:32:08 -0500

    annual salary adds up at bottom of page

--- javaScript.js
+++ javaScript.js
@@ -1,22 +1,37 @@
+
+
+let total = 0;
+
 onReady();
 
 function onReady(){
-    console.log('JS is working');
+    console.log('JS is working', total);
+
+    // renderMoneyCalculator();
 }
+
 
 
 
 
 function onSubmit(event){
     event.preventDefault();
-    let element = event.target;
-    const firstName = document.getElementById('firstName');
-    const lastName = document.getElementById('lastName');
-    const id = document.getElementById('id');
-    const title = document.getElementById('title');
-    const annualSalary = document.getElementById('annualSalary');
-    const tr = document.getElementById('tr');
+    const element = event.target;
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const id = document.getElementById('id').value;
+    const title = document.getElementById('title').value;
+    const annualSalary = document.getElementById('annualSalary').value;
+    console.log(annualSalary);
+    // const tr = document.getElementById('tr');
 
+    
+    const paraElement = document.getElementById('remaining');
+    console.log(paraElement);
+    total += Number(annualSalary) / 12;
+    console.log('what is the total', total);
+    // paraElement.innerText = total;
+    paraElement.innerText = `$${total} Monthly Total`;
     document.getElementById('tbody').innerHTML += `
     <tr>
     <td>${firstName}</td>
@@ -24,10 +39,37 @@ function onSubmit(event){
     <td>${id}</td>
     <td>${title}</td>
     <td>${annualSalary}</td>
-    <td><button id="removeButton">❌</button></td>
+    <td><button id="removeButton" onclick="removeButton(event)">❌</button></td>
     </tr>
     `
 
-
     document.getElementById('form').reset();
 }
+
+function removeButton(event){
+    const element = event.target;
+    element.closest('tr').remove();
+    console.log('ncndfjfj');
+}
+
+// function moneyCalculator(){
+    
+// }
+
+
+// function renderMoneyCalculator(){
+//     // const remaining = moneyCalculator();
+//     // const h5 = document.getElementById('remaining');
+//     console.log('fjjrj');
+//     // let total = 0;
+//     // const paraElement = document.getElementById('remaining');
+//     // console.log(paraElement);
+//     // paraElement.innerText = total;
+//     // for(const salary of employees){
+//     //     total += Number(salary.employees);
+//     // }
+    
+//     // let sum = total / 12;
+//     // console.log(sum);
+//     // return sum;
+// }
